f42b1b561faf5e9c6cd0ded38ad12f788edcb268
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-08 22:35:28 +0800

    input component

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { colors, fonts } from '../../../utils';
+
+interface InputType {
+  label: string;
+  value?: any;
+  onChangeText?: any;
+  secureTextEntry?: any;
+  disable?: boolean;
+}
+
+const Input = ({ label, value, onChangeText, secureTextEntry, disable }: InputType) => {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+  const onBlurForm = () => {
+    setBorder(colors.border);
+  };
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={styles.input(border)}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        editable={!disable}
+        selectTextOnFocus={!disable}
+      />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  input: (border) => ({
+    borderWidth: 1,
+    borderColor: border,
+    borderRadius: 10,
+    padding: 12
+  }),
+  label: {
+    fontSize: 16,
+    color: colors.text.secondary,
+    marginBottom: 6,
+    fontFamily: fonts.primary[400]
+  }
+});
--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,2 +1,3 @@
 export { default as Button } from './Button';
 export { default as Gap } from './Gap';
+export { default as Input } from './Input';
