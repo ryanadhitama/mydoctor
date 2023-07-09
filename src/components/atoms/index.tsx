5142f58747b39da56cad14212250a930d71ea7ab
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-09 19:16:48 +0800

    tab item

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Gap } from './Gap';
 export { default as Input } from './Input';
 export { default as Link } from './Link';
+export { default as TabItem } from './TabItem';
