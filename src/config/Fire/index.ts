3dc21090a79b28d055fca54a2e3525f5fea439de
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-16 22:17:59 +0800

    update config

--- src/config/Fire/index.ts
+++ src/config/Fire/index.ts
@@ -1,18 +1,9 @@
 import firebase from 'firebase';
 
-// firebase.initializeApp({
-//   apiKey: 'AIzaSyAEsqrtHr1kt9zm3DNsDscEx02mDccgVJY',
-//   authDomain: 'my-doctor-01-5204e.firebaseapp.com',
-//   databaseURL: 'https://my-doctor-01-5204e.firebaseio.com',
-//   projectId: 'my-doctor-01-5204e',
-//   storageBucket: 'my-doctor-01-5204e.appspot.com',
-//   messagingSenderId: '399645842046',
-//   appId: '1:399645842046:web:23eef2493ad4fe6f68d62f'
-// });
-
 firebase.initializeApp({
   apiKey: 'AIzaSyDY1LD3sQCpyHRhL9nxLJ5Wy6M9kXVQwcQ',
   authDomain: 'mydoctor-5d3bb.firebaseapp.com',
+  databaseURL: 'https://mydoctor-5d3bb-default-rtdb.firebaseio.com',
   projectId: 'mydoctor-5d3bb',
   storageBucket: 'mydoctor-5d3bb.appspot.com',
   messagingSenderId: '982743034587',
