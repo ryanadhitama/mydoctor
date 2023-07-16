015da47b17729c146018f12448a10ce687c1e09a
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-16 22:12:33 +0800

    add firebase

---
+++ src/config/Fire/index.ts
@@ -0,0 +1,25 @@
+import firebase from 'firebase';
+
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyAEsqrtHr1kt9zm3DNsDscEx02mDccgVJY',
+//   authDomain: 'my-doctor-01-5204e.firebaseapp.com',
+//   databaseURL: 'https://my-doctor-01-5204e.firebaseio.com',
+//   projectId: 'my-doctor-01-5204e',
+//   storageBucket: 'my-doctor-01-5204e.appspot.com',
+//   messagingSenderId: '399645842046',
+//   appId: '1:399645842046:web:23eef2493ad4fe6f68d62f'
+// });
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDY1LD3sQCpyHRhL9nxLJ5Wy6M9kXVQwcQ',
+  authDomain: 'mydoctor-5d3bb.firebaseapp.com',
+  projectId: 'mydoctor-5d3bb',
+  storageBucket: 'mydoctor-5d3bb.appspot.com',
+  messagingSenderId: '982743034587',
+  appId: '1:982743034587:web:0a5136f148a1e190223afe',
+  measurementId: 'G-FK69TTZ0JT'
+});
+
+const Fire = firebase;
+
+export default Fire;
