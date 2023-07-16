6112b5171a886f599d3ff4fe9682f11e5c2b8a31
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-16 21:55:34 +0800

    chat item

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,9 +1,10 @@
 export { default as BottomNavigator } from './BottomNavigator';
+export { default as ChatItem } from './ChatItem';
 export { default as DoctorCategory } from './DoctorCategory';
 export { default as Header } from './Header';
 export { default as HomeProfile } from './HomeProfile';
+export { default as List } from './List';
 export { default as ListHospital } from './ListHospital';
 export { default as NewsItem } from './NewsItem';
-export { default as RatedDoctor } from './RatedDoctor';
-export { default as List } from './List';
 export { default as Profile } from './Profile';
+export { default as RatedDoctor } from './RatedDoctor';
