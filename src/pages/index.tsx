40c92b1ca12e50db9ddfdb0edd698c43843abe73
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-21 22:24:10 +0800

    reorder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,6 @@ export { default as Hospitals } from './Hospitals';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as Splash } from './Splash';
+export { default as UpdateProfile } from './UpdateProfile';
 export { default as UploadPhoto } from './UploadPhoto';
 export { default as UserProfile } from './UserProfile';
-export { default as UpdateProfile } from './UpdateProfile';
