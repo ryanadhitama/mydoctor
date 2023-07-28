29e19c1ba9929b5dbf057b537865cfb80918fc05
ryanadhitama <ryanadhitama2@gmail.com>
2023-07-28 08:35:53 +0800

    reorder page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
+export { default as Chatting } from './Chatting';
+export { default as ChooseDoctor } from './ChooseDoctor';
 export { default as Doctor } from './Doctor';
+export { default as DoctorProfile } from './DoctorProfile';
 export { default as GetStarted } from './GetStarted';
 export { default as Hospitals } from './Hospitals';
 export { default as Login } from './Login';
+export { default as Messages } from './Messages';
 export { default as Register } from './Register';
 export { default as Splash } from './Splash';
 export { default as UpdateProfile } from './UpdateProfile';
 export { default as UploadPhoto } from './UploadPhoto';
 export { default as UserProfile } from './UserProfile';
-export { default as DoctorProfile } from './DoctorProfile';
-export { default as Chatting } from './Chatting';
-export { default as Messages } from './Messages';
-export { default as ChooseDoctor } from './ChooseDoctor';
